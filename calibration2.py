4597883ebcdf9dc0dd588e1df327bbf7f616517a
ChrisCheang <128516153+ChrisCheang@users.noreply.github.com>
2025-05-27 22:41:26 +0100

    Update calibration2.py

--- calibration2.py
+++ calibration2.py
@@ -15,7 +15,7 @@ global t_sleep, csv_writer
 t_sleep = 0.05
 csv_writer = None
 
-initialized = False
+initialized = True
 
 
 odrv0 = odrive.find_any() # Reconnect to the Odrive
@@ -32,38 +32,9 @@ if not initialized:
     odrv0.axis0.motor.config.pole_pairs = 11
     odrv0.axis1.motor.config.pole_pairs = 11
 
-    odrv0.save_configuration()
-    
-    print("starting motor 0 axis state motor calibration")
-    odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE  #AXIS_STATE_MOTOR_CALIBRATION (see if this is the problem)
-    while odrv0.axis0.current_state != AXIS_STATE_IDLE: # Wait for calibration to be done
-        time.sleep(0.1)
-        print(".", end="")
-
-    print("starting motor 0 axis state encoder offset calibration")
-    odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
-    while odrv0.axis0.current_state != AXIS_STATE_IDLE: # Wait for calibration to be done
-        time.sleep(0.1)
-        print(".", end="")
-
-    print("starting motor 0 axis state homing (current limit)")
-    odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    odrv0.axis0.controller.config.vel_ramp_rate = 0.5
-    odrv0.axis0.motor.config.current_lim = 30
-    odrv0.axis0.controller.config.input_mode = INPUT_MODE_VEL_RAMP
-    odrv0.axis0.controller.input_vel = 500
-    
-    while round(odrv0.axis0.motor.current_control.Iq_measured,2)<3: # Wait for calibration to be done
-        #time.sleep(0.1)
-        print("current: ",round(odrv0.axis0.motor.current_control.Iq_measured,2))
-        
-    odrv0.axis0.controller.input_vel = 0
-    odrv0.axis0.requested_state = AXIS_STATE_IDLE
-    odrv0.axis0.encoder.set_linear_count(0)
+    #odrv0.save_configuration() for some reason save config line here disconnects it (27-5)
 
 
-
-    print("Motor 0: Homing complete")
 
     print("starting motor 1 axis state motor calibration")
     odrv0.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE  #AXIS_STATE_MOTOR_CALIBRATION (see if this is the problem)
@@ -98,7 +69,38 @@ if not initialized:
 
 
 
+    print("starting motor 0 axis state motor calibration")
+    odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE  #AXIS_STATE_MOTOR_CALIBRATION (see if this is the problem)
+    while odrv0.axis0.current_state != AXIS_STATE_IDLE: # Wait for calibration to be done
+        time.sleep(0.1)
+        print(".", end="")
+
+    print("starting motor 0 axis state encoder offset calibration")
+    odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+    while odrv0.axis0.current_state != AXIS_STATE_IDLE: # Wait for calibration to be done
+        time.sleep(0.1)
+        print(".", end="")
+
+    print("starting motor 0 axis state homing (current limit)")
+    odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+    odrv0.axis0.controller.config.vel_ramp_rate = 0.5
+    odrv0.axis0.motor.config.current_lim = 30
+    odrv0.axis0.controller.config.input_mode = INPUT_MODE_VEL_RAMP
+    odrv0.axis0.controller.input_vel = 500
+    
+    while round(odrv0.axis0.motor.current_control.Iq_measured,2)<3: # Wait for calibration to be done
+        #time.sleep(0.1)
+        print("current: ",round(odrv0.axis0.motor.current_control.Iq_measured,2))
+        
+    odrv0.axis0.controller.input_vel = 0
+    odrv0.axis0.requested_state = AXIS_STATE_IDLE
+    odrv0.axis0.encoder.set_linear_count(0)
+
+    print("Motor 0: Homing complete")
+
+
     initialized = True
+
 
 print("move to middle")
 
@@ -109,17 +111,17 @@ while True:
     odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
     
     odrv0.axis0.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ
-    odrv0.axis0.trap_traj.config.vel_limit = 10
-    odrv0.axis0.trap_traj.config.accel_limit = 10
-    odrv0.axis0.trap_traj.config.decel_limit = 10
+    odrv0.axis0.trap_traj.config.vel_limit = 200
+    odrv0.axis0.trap_traj.config.accel_limit = 300
+    odrv0.axis0.trap_traj.config.decel_limit = 300
     odrv0.axis0.controller.input_pos = -5
 
     odrv0.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
     odrv0.axis1.controller.config.input_mode = INPUT_MODE_TRAP_TRAJ
-    odrv0.axis1.trap_traj.config.vel_limit = 10
-    odrv0.axis1.trap_traj.config.accel_limit = 10
-    odrv0.axis1.trap_traj.config.decel_limit = 10
+    odrv0.axis1.trap_traj.config.vel_limit = 200
+    odrv0.axis1.trap_traj.config.accel_limit = 300
+    odrv0.axis1.trap_traj.config.decel_limit = 300
     odrv0.axis1.controller.input_pos = -5
 
 
