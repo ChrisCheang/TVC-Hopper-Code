4ae70675bd10b58a983d92155d16e8293fc0aa89
ChrisCheang <128516153+ChrisCheang@users.noreply.github.com>
2025-06-14 16:31:04 +0100

    Update udp_test_send.py
    Added dual direction sending capability (response)

--- udp_test_send.py
+++ udp_test_send.py
@@ -10,6 +10,7 @@ import numpy as np
 
 start = time.time()
 
+global response
 
 class JSONSenderProtocol(asyncio.DatagramProtocol):
     def __init__(self):
@@ -20,6 +21,14 @@ class JSONSenderProtocol(asyncio.DatagramProtocol):
         self.transport = transport
         asyncio.create_task(self.send_loop())
 
+    def datagram_received(self, data, addr):
+        try:
+            response = json.loads(data.decode())
+            timestamp = response["timestamp"]
+            print(f"Received: {timestamp}")
+        except json.JSONDecodeError:
+            print(f"Invalid response from {addr}: {data.decode()}")
+
     async def send_loop(self):
         while True:
             now = time.time()
@@ -29,8 +38,8 @@ class JSONSenderProtocol(asyncio.DatagramProtocol):
                     "tvc0":{
                         "channel":0,
                         "state":"calibrate", #idle, calibrate, arm, lock, test_procedure, demand_pos
-                        "gimbal_angle_0":0*np.pi/180,
-                        "gimbal_angle_1":0*np.pi/180
+                        "gimbal_angle_0":5*np.pi/180,
+                        "gimbal_angle_1":5*np.pi/180
                     }
                 },
                 "timestamp": f"{now}",
@@ -38,7 +47,9 @@ class JSONSenderProtocol(asyncio.DatagramProtocol):
             }
             data = json.dumps(tvcinput).encode()
             self.transport.sendto(data)
-            print(f"Sent: {tvcinput}")
+            timestamp = float(tvcinput["timestamp"])
+            state = tvcinput["tvcs"]["tvc0"]["state"]
+            print(f"Sent: tvcinput, {timestamp}, {state}")
             self.counter += 1
             await asyncio.sleep(0.001)  # Need to keep this or else the time between send and recieve jumps to 0.1s
 
@@ -50,13 +61,13 @@ class JSONSenderProtocol(asyncio.DatagramProtocol):
 
 
 async def main():
-    print("Sending UDP message...")
-
+    print("Starting UDP sender...")
     loop = asyncio.get_running_loop()
 
     transport, protocol = await loop.create_datagram_endpoint(
         lambda: JSONSenderProtocol(),
-        remote_addr=('127.0.0.1', 9999)
+        local_addr=('127.0.0.1', 9998),  # Local port to receive responses
+        remote_addr=('127.0.0.1', 9999)  # Receiver's port
     )
 
     try:
